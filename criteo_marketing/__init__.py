a4d636724fe4dfa04394ae5b94938650dc3d4610
Travis CI <bud-team@criteo.com>
2020-08-19 13:02:11 +0000

    Automatic update of SDK - v1.0.170

--- criteo_marketing/__init__.py
+++ criteo_marketing/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "1.0.169"
+__version__ = "1.0.170"
 
 # import apis into sdk package
 from criteo_marketing.api.advertisers_api import AdvertisersApi
